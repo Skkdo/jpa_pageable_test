5560b583d25b40c6ec25d6623f0b388d7c5f950e
박진형 <skill1218@naver.com>
2025-02-26 16:04:52 +0900

    project init

---
+++ pageable/src/main/java/jpa_test/pageable/PageableApplication.java
@@ -0,0 +1,13 @@
+package jpa_test.pageable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PageableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PageableApplication.class, args);
+	}
+
+}
